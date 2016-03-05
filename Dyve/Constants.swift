0a9f3d1c989b189f33ef05f1e6d6c1d331c80ed8
anashmamouch <anashmamouch90@gmail.com>
2016-03-05 14:47:13 +0000

    added facebook login into app

---
+++ Dyve/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Dyve
+//
+//  Created by Anas Hmamouch on 4/3/16.
+//  Copyright © 2016 Benzino. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//Global constants used by all the application
+
+let SHADOW_COLOR: CGFloat = 157.0/255.0
--- Dyve/ViewController.swift
+++ Dyve/ViewController.swift
@@ -7,19 +7,37 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func fbButtonPressed(sender: UIButton) {
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logInWithReadPermissions(["email"], fromViewController: self, handler: {(facebookResult, facebookError) -> Void in
+            
+            if facebookError != nil {
+                print("Facebook login error: \(facebookError)")
+            }else if facebookResult.isCancelled {
+            
+            }else {
+                let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+                print("Successfully logged in with facebook: \(accessToken) ")
+                
+                
+            
+            }
+        
+        })
     }
 
-
+    
+    
 }
 
